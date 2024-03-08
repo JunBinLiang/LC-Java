dde4fc8fe99fe9a9b5c4da1e8daa46636d042c6d
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-03-07 19:25:38 -0500

    Update Maximize Score After N Operations.java

--- Maximize Score After N Operations.java
+++ Maximize Score After N Operations.java
@@ -38,3 +38,46 @@ class Solution {
         return dp[st] = ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int maxScore(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[(1 << n) + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+    
+        for(int st = 1; st < (1 << n); st++) {
+            int count = Integer.bitCount(st);
+            if(count % 2 == 1) continue;
+            for(int i = 0; i < n; i++) {
+                for(int j = i + 1; j < n; j++) {
+                    if(((st & (1 << i)) > 0) && ((st & (1 << j)) > 0)) {
+                        int last = st ^ (1 << i);
+                        last ^= (1 << j);
+                        if(dp[last] != -1) {
+                            dp[st] = Math.max(dp[st], dp[last] + gcd(nums[i], nums[j]) * (count / 2));
+                        }
+                            
+                    }
+                }
+            }
+        }
+        return dp[(1 << n) - 1];
+    }
+    
+    public int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        else
+            return gcd(b, a % b);
+    }
+}
