07783285884bf5503a2f08e9397ad3a3ffeff77d
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-03-28 21:16:46 -0400

    Create Corporate Flight Bookings.java

---
+++ Corporate Flight Bookings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] diff = new int[n + 1];
+        for(int[] p : bookings) {
+            p[0]--;
+            p[1]--;
+            diff[p[0]] += p[2];
+            diff[p[1] + 1] -= p[2];
+        }
+        
+        int[] ans = new int[n];
+        int s = 0;
+        for(int i = 0; i < n; i++) {
+            s += diff[i];
+            ans[i] = s;
+        }
+        return ans;
+    }
+}
